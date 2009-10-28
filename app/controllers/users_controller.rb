8f131c774ae949df2d98c3e632999af9ce912cd0
Christian Beier <cb@medienlaborpotsdam.de>
2009-10-28 09:49:34 +0100

    fix bug, so that the application in production mode does not break down, if the users controller is overridden

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < BaseController
     end
   end    
 
+  require File.dirname(__FILE__) + '/../../config/initializers/mce_options.rb'
   uses_tiny_mce(:options => AppConfig.default_mce_options.merge({:editor_selector => "rich_text_editor"}), 
     :only => [:new, :create, :update, :edit, :welcome_about])
   uses_tiny_mce(:options => AppConfig.simple_mce_options, :only => [:show])
