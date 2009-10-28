ea8d4304afbd2cde3ecdce13b2991c8a80095e5d
Christian Beier <cb@medienlaborpotsdam.de>
2009-10-28 09:55:14 +0100

    cosmetical correction: load the required file only if the variable is blank

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < BaseController
     end
   end    
 
-  require File.dirname(__FILE__) + '/../../config/initializers/mce_options.rb'
+  require File.dirname(__FILE__) + '/../../config/initializers/mce_options.rb' if AppConfig.default_mce_options.blank?
   uses_tiny_mce(:options => AppConfig.default_mce_options.merge({:editor_selector => "rich_text_editor"}), 
     :only => [:new, :create, :update, :edit, :welcome_about])
   uses_tiny_mce(:options => AppConfig.simple_mce_options, :only => [:show])
